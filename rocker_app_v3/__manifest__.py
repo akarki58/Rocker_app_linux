45a13dfaf8aca77d54ec85adf70041c780d18ecc
Akarki58 <antti.karki@iki.fi>
2025-09-13 13:19:54 +0300

    Set price

--- rocker_app_v3/__manifest__.py
+++ rocker_app_v3/__manifest__.py
@@ -3,7 +3,9 @@
  'description': 'Get data from Odoo & external PostgreSQL, SQLServer, MySQL, MariaDB, ODBC or Oracle databases.',
  'author': 'Antti Kärki',
  'depends': ['base','web','mail'],
- 'license': 'AGPL-3',
+ 'license': 'OPL-1',
+ 'price': 10.00,
+ 'currency': 'EUR',
  'category': 'Rocker/Reporting',
  'version': '16.0.3.0.1',
  'data': [
