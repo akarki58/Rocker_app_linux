6773c540b2b62b3b192443afa9e762df3a110c15
Akarki58 <antti.karki@iki.fi>
2026-01-22 11:43:47 +0200

    Version 19.0.1.0

--- rocker_app_v3/__manifest__.py
+++ rocker_app_v3/__manifest__.py
@@ -8,7 +8,7 @@
  'currency': 'EUR',
  'category': 'Rocker/Reporting',
  'sequence': 23,
- 'version': '18.0.1.0',
+ 'version': '19.0.1.0',
  'data': [
   'security/rocker_security.xml',
   'security/ir.model.access.csv',
--- rocker_app_v3/models/rocker_database.py
+++ rocker_app_v3/models/rocker_database.py
@@ -13,7 +13,8 @@
 #    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
 #
 ##
-# 2025-01-23
+# 2026-01-17
+
 #
 #############################################################################
 
@@ -33,7 +34,7 @@ class rocker_database(models.Model):
     _sql_constraints = [('unique_name', 'UNIQUE(name)', 'Datasource Name must be unique')]
     driver = fields.Selection(
         [('mysql', 'MySQL'), ('mariadb', 'MariaDB'), ('oracle', 'Oracle'), ('sqlserver', 'SQLServer'), ('odbc', 'ODBC'),
-         ('postgresql', 'PostrgeSQL'), ('sqlalchemy', 'SQLAlchemy')], 'Driver', required=True, default='postgresql')
+         ('postgresql', 'PostrgeSQL'), ('sqlalchemy', 'SQLAlchemy')], 'Driver', required=True, default='sqlalchemy')
     sqlalchemydriver = fields.Char('SQLAlchemy driver', required=False, default='postgresql+psycopg2')
     odbcdriver = fields.Char('ODBC driver', required=False, default='SQL Server')
     # sid = fields.Char('Oracle SID', required=False)
